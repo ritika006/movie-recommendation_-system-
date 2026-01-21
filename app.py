56773eeaedc8bf58c9725717356a8db5f70bf747
ritika006 <sharitika9116@gmail.com>
2026-01-21 21:00:46 +0530

    Initial Streamlit app

--- app.py
+++ app.py
@@ -2,74 +2,63 @@ import streamlit as st
 import pickle
 import requests
 
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get("poster_path")
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    return ""
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+# Load data
+movies = pickle.load(open("movies_list.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
-st.header("Movie Recommender System")
+movies_list = movies["title"].values
 
-import streamlit.components.v1 as components
+st.title("🎬 Movie Recommendation System")
 
-imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
+# --- Top Posters (Carousel replacement) ---
+st.subheader("🔥 Popular Movies")
 
+poster_ids = [
+    1632, 299536, 17455, 2830, 429422,
+    9722, 13972, 240, 155, 598
+]
 
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-   
-    ]
+cols = st.columns(5)
+for i, movie_id in enumerate(poster_ids):
+    with cols[i % 5]:
+        st.image(fetch_poster(movie_id), use_container_width=True)
 
-
-imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
-
+# --- Recommendation Logic ---
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
-    for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
-        recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
-    return recommend_movie, recommend_poster
+    index = movies[movies["title"] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
 
+    recommended_movies = []
+    recommended_posters = []
 
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
 
-if st.button("Show Recommend"):
-    movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
-    with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
-    with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
-    with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
-    with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
-    with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
+    return recommended_movies, recommended_posters
+
+# --- UI ---
+selected_movie = st.selectbox("🎥 Select a movie", movies_list)
+
+if st.button("✨ Show Recommendations"):
+    names, posters = recommend(selected_movie)
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i], use_container_width=True)
